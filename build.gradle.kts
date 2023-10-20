e359f8f0c2c4f4c5b011412a094f69483adeefa6
mmoya064 <mmoya064@uottawa.ca>
2023-10-19 21:48:41 -0400

    Initial commit

---
+++ build.gradle.kts
@@ -0,0 +1,15 @@
+buildscript {
+    val kotlin_version by extra("1.9.20-RC")
+    dependencies {
+        classpath("com.google.gms:google-services:4.4.0")
+        classpath("org.jetbrains.kotlin:kotlin-gradle-plugin:$kotlin_version")
+    }
+    repositories {
+        mavenCentral()
+    }
+}
+// Top-level build file where you can add configuration options common to all sub-projects/modules.
+plugins {
+    id("com.android.application") version "8.1.2" apply false
+    id("com.google.gms.google-services") version "4.4.0" apply false
+}
